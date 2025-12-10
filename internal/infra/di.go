1725fcc2cc9a9c258438b7deca95b3b80fa60a31
João Victor Oliveira Couto <commits@jictyvoo.dev>
2025-12-10 03:28:06 -0300

    :syringe: feat: Include new repositories on dependency injection

--- internal/infra/di.go
+++ internal/infra/di.go
@@ -3,9 +3,11 @@ package infra
 import (
 	"github.com/wrapped-owls/goremy-di/remy"
 
+	"github.com/jictyvoo/amigonimo_api/internal/infra/facades/mailfacade"
 	"github.com/jictyvoo/amigonimo_api/internal/infra/repositories"
 )
 
 func RegisterInfraServices(inj remy.Injector) {
 	repositories.RegisterRepositories(inj)
+	remy.RegisterConstructor(inj, remy.Singleton[*mailfacade.MailerImpl], mailfacade.NewMailerImpl)
 }
--- internal/infra/repositories/di.go
+++ internal/infra/repositories/di.go
@@ -2,11 +2,24 @@ package repositories
 
 import (
 	"github.com/jictyvoo/amigonimo_api/internal/infra/repositories/mysqlrepo"
+	"github.com/jictyvoo/amigonimo_api/internal/infra/repositories/mysqlrepo/authtokenrepo"
+	"github.com/jictyvoo/amigonimo_api/internal/infra/repositories/mysqlrepo/authuserepo"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/wrapped-owls/goremy-di/remy"
 )
 
 func RegisterRepositories(inj remy.Injector) {
+	// Base SQL connection wrapper
 	remy.RegisterConstructorArgs1(inj, remy.Factory[mysqlrepo.RepoMySQL], mysqlrepo.NewRepoMySQL)
+
+	// Start to inject all other constructors
+	remy.RegisterConstructorArgs1(
+		inj, remy.Factory[*authuserepo.RepoMySQL],
+		authuserepo.NewRepoMySQL,
+	)
+	remy.RegisterConstructorArgs1(
+		inj, remy.Factory[*authtokenrepo.RepoMySQL],
+		authtokenrepo.NewRepoMySQL,
+	)
 }
