0cf11f2fd0f9ebe7b36b6c82fa522bf7dfa79c28
João Victor Oliveira Couto <commits@jictyvoo.dev>
2025-12-10 01:00:37 -0300

    :custard: feat(auth): Implement user edition service

---
+++ internal/domain/authcore/userserv/interface_facades.go
@@ -0,0 +1,14 @@
+package userserv
+
+import (
+	"github.com/jictyvoo/amigonimo_api/internal/entities"
+)
+
+//go:generate mockgen -destination=../../mocks/useredit_repo_mock.go -package=mocks github.com/jictyvoo/amigonimo_api/internal/domain/authcore/userserv UserEditionRepository
+
+type UserEditionRepository interface {
+	GetUserByAuthCode(authToken string) (entities.User, error)
+	ChangeEmail(userID entities.HexID, newEmail string) error
+	SetNewVerificationCode(userID entities.HexID, code string) error
+	UpdateUsername(userID entities.HexID, username string) error
+}
