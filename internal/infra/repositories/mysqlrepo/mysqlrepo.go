a06b7bf03a9a38fdba0500f2ae4da781ab527840
João Victor Oliveira Couto <commits@jictyvoo.dev>
2025-12-24 17:15:40 -0300

    :honey_pot: feat: Add database transaction management

--- internal/infra/repositories/mysqlrepo/mysqlrepo.go
+++ internal/infra/repositories/mysqlrepo/mysqlrepo.go
@@ -8,6 +8,10 @@ import (
 	"github.com/jictyvoo/amigonimo_api/internal/infra/repositories/mysqlrepo/internal/dbgen"
 )
 
+type OnFinishFunc func(commit bool) error
+
+// RepoMySQL is the main type to be used to perform database operations.
+// It provides wrappers for every required
 type RepoMySQL struct {
 	conn    *sql.DB
 	queries *dbgen.Queries
@@ -17,14 +21,35 @@ func NewRepoMySQL(db *sql.DB) RepoMySQL {
 	return RepoMySQL{conn: db, queries: dbgen.New(db)}
 }
 
-func (r RepoMySQL) Ctx() (context.Context, context.CancelFunc) {
+func (r *RepoMySQL) Ctx() (context.Context, context.CancelFunc) {
 	return context.WithTimeout(context.Background(), 30*time.Second)
 }
 
-func (r RepoMySQL) Connection() *sql.DB {
+func (r *RepoMySQL) Connection() *sql.DB {
 	return r.conn
 }
 
-func (r RepoMySQL) Queries() *dbgen.Queries {
+func (r *RepoMySQL) Queries() *dbgen.Queries {
 	return r.queries
+}
+
+func (r *RepoMySQL) BeginTx(ctx context.Context, txOpts *sql.TxOptions) (OnFinishFunc, error) {
+	tx, err := r.conn.BeginTx(ctx, txOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	r.queries = r.queries.WithTx(tx)
+	txf := transactionFinisher{tx}
+	return txf.Finish, nil
+}
+
+type transactionFinisher struct{ *sql.Tx }
+
+func (tx transactionFinisher) Finish(commit bool) error {
+	if commit {
+		return tx.Commit()
+	}
+
+	return tx.Rollback()
 }
