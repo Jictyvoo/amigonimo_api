55f832d9ebdf2a505ce156863fda68261dcf5da6
João Victor Oliveira Couto <commits@jictyvoo.dev>
2026-01-17 16:36:10 -0300

    :wrench: feat(drawserv): Implement core secret friend drawing algorithm

---
+++ internal/domain/services/drawserv/errors.go
@@ -0,0 +1,8 @@
+package drawserv
+
+import "errors"
+
+var (
+	ErrNoValidDraw         = errors.New("no valid drawfriends found after maximum attempts")
+	ErrInsufficientPlayers = errors.New("at least 3 participants are required for a drawfriends")
+)
