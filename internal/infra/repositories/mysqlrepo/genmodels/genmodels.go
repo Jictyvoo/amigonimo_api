18cfd9134d55d4a9717f4e1061b9436930dff8ea
João Victor Oliveira Couto <commits@jictyvoo.dev>
2026-01-26 00:05:53 -0300

    :closed_umbrella: feat(mysqlrepo): Introduce `genmodels` type aliases

---
+++ internal/infra/repositories/mysqlrepo/genmodels/genmodels.go
@@ -0,0 +1,13 @@
+package genmodels
+
+import "github.com/jictyvoo/amigonimo_api/internal/infra/repositories/mysqlrepo/internal/dbgen"
+
+type (
+	AuthToken    = dbgen.AuthToken
+	Denylist     = dbgen.Denylist
+	DrawResult   = dbgen.DrawResult
+	Participant  = dbgen.Participant
+	SecretFriend = dbgen.SecretFriend
+	User         = dbgen.User
+	WishlistItem = dbgen.WishlistItem
+)
